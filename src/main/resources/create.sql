5a5ed8f624a335aa52328f4bc7dc1e86ae0ff259
harperzhu <harperzhu@yahoo.com>
2022-03-14 21:21:57 -0700

    okay

---
+++ src/main/resources/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Caregivers (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Availabilities (
+    Time date,
+    Username varchar(255) REFERENCES Caregivers,
+    PRIMARY KEY (Time, Username)
+);
+
+CREATE TABLE Vaccines (
+    Name varchar(255),
+    Doses int,
+    PRIMARY KEY (Name)
+);
+
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Booked (
+    id INT IDENTITY(1,1), -- auto-increments ID
+    Time date,
+    Username varchar(255) REFERENCES Caregivers,
+    Patient varchar(255) REFERENCES Patients,
+    v_name varchar(255) REFERENCES Vaccines,
+    PRIMARY KEY (id)
+);
